6ce2fdf7d018d300f76c21457a2896983e3ec58a
Subrato <subrato.sarker@welldev.io>
2020-05-23 05:44:27 +0600

    :dog:

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module PracticeAPI
     config.api_only = false
     config.autoload_paths << "#{Rails.root}/lib"
     config.assets.paths << Rails.root.join('app', 'assets', 'img')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,17 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  # config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -55,7 +55,8 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  config.assets.debug = true
+  config.assets.quiet = true
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
